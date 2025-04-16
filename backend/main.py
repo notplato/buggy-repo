80d3eeba2457d4da933a804842a3951cbc349210
Kaushik Yadala <yadala.k1@gmail.com>
2025-04-16 16:00:10 +0530

    kaushik fix 2

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request  # add request to use
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from routes.analytics import router as analytics_router
 from routes.items import router as items_router
 from routes.quiz import router as quiz_router
@@ -9,9 +11,9 @@ app.include_router(items_router, prefix="/items")
 app.include_router(analytics_router)
 app.include_router(quiz_router)
 
+templates = Jinja2Templates(directory="../frontend/")
 
-# why the hell did I write this function?
-@app.get("/home")
-async def get_home():
-    return {"message": "Welcome to the Multi-Page FastAPI App!"}
 
+@app.get("/home", response_class=HTMLResponse)
+async def get_home(request: Request):  # modify to show index.html
+    return templates.TemplateResponse("index.html", {"request": request})
