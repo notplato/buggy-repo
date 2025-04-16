725d393ebcabe581c7c138996bf087a0f5ebe39c
Kaushik Yadala <yadala.k1@gmail.com>
2025-04-16 17:40:28 +0530

    kaushik fix 6

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 
-class Item(BaseModel): # Needs to inherit from BaseModel
-    name: int # Name needs to be a string
+
+class Item(BaseModel):  # Needs to inherit from BaseModel
+    name: str  # Name needs to be a string
     description: str
+
 
 class User(BaseModel):
     username: str
     bio: str
-    
+
     # You can raise your hands and give the answer to the chocolate question
+
